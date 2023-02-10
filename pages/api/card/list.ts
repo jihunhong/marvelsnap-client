b6d6755ad3dc47d3f855eb093240fef8797a7a30
jihunhong <redgee49@gmail.com>
2023-02-11 06:15:34 +0900

    /api/card/list 캐싱

--- pages/api/card/list.ts
+++ pages/api/card/list.ts
@@ -9,5 +9,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     sort: '+cost,+power,+en',
     filter: 'grade!=0',
   });
+  res.setHeader('Cache-Control', 's-maxage=86400');
   res.status(200).json(cards);
 }
