f26c8c8e65a4b9202d9e9724c89b326fbc18524f
jihunhong <redgee49@gmail.com>
2023-02-13 01:39:20 +0900

    privacy, terms

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -9,7 +9,7 @@ const Privacy = () => {
   return (
     <>
       <PageIntro title="Privacy" description="" bgSource={backgroundUrls.login} />
-      <section>
+      <section className="privacy">
         snapsco는 개인정보 보호법 제30조에 따라 정보 주체의 개인정보를 보호하고 이와 관련한 고충을 신속하고 원활하게 처리할 수 있도록 하기 위하여 다음과 같이 개인정보 처리지침을 수립, 공개합니다.
         <br />
         <br />
