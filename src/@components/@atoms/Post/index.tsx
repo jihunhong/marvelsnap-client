531674e9191c350e269ecee69a4f43cc20d40765
jihunhong <redgee49@gmail.com>
2023-02-22 01:25:31 +0900

    Post Image ObjectFit 수정

--- src/@components/@atoms/Post/index.tsx
+++ src/@components/@atoms/Post/index.tsx
@@ -1,5 +1,3 @@
-import Avatar from '@atoms/Avatar';
-import Divider from '@atoms/Divider';
 import Image from 'next/image';
 import { PostPreviewContainer } from './style';
 
@@ -17,14 +15,13 @@ const Post = ({ title = 'ChatGPT Is Having a Thomas Edison Moment', summary = 'W
       <PostPreviewContainer>
         <div className="meta">
           <div className="writer">
-            {/* <Avatar src="https://avatars.githubusercontent.com/u/21700764?v=4" /> */}
             <h4>{writer}</h4>
           </div>
           <h2>{title}</h2>
           <h3>{summary}</h3>
         </div>
         <div className="thumb-container">
-          <Image width={200} height={134} src={thumbnail!} alt={title || 'post thumbnail'} />
+          <Image width={200} height={134} src={thumbnail!} alt={title || 'post thumbnail'} objectFit="cover" />
         </div>
       </PostPreviewContainer>
     </>
