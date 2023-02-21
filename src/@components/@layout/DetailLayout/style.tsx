e71cf15b68aa3d72842f9f6c4e16ea4d8a4d75b7
jihunhong <redgee49@gmail.com>
2023-02-22 02:39:04 +0900

    article detail 조금 수정

--- src/@components/@layout/DetailLayout/style.tsx
+++ src/@components/@layout/DetailLayout/style.tsx
@@ -1,7 +1,17 @@
+import { RawHtmlContainer } from '@atoms/RawHtml/style';
 import styled from 'styled-components';
 
 // prettier - ignore
 export const DetailLayoutContainer = styled.section`
   display: flex;
   flex-direction: column;
+
+  .raw {
+    ${RawHtmlContainer} {
+      p:has(img) {
+        display: flex;
+        justify-content: center;
+      }
+    }
+  }
 `;
