b5419405a69b9ca0be599e0d027a241271b2743b
jihunhong <redgee49@gmail.com>
2023-02-27 06:56:35 +0900

    스타일 수정

--- src/@components/@template/article/style.tsx
+++ src/@components/@template/article/style.tsx
@@ -4,6 +4,7 @@ import { MarkdownCss } from '@styles/markdown';
 export const ArticleTemplateContainer = styled.div`
     .contents {
         ${MarkdownCss}
+        margin-bottom: 2rem;
     }
 
     .actions {
