21feb4e872f84933cd2d1ee513c5cae756ac78c5
jihunhong <redgee49@gmail.com>
2023-04-25 22:20:51 +0900

    메타덱 리스트 redis disable

--- pages/api/meta/deck/list.ts
+++ pages/api/meta/deck/list.ts
@@ -1,5 +1,3 @@
-import redis from '@lib/redis';
-import keys from '@query/keys';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import PocketBase from 'pocketbase';
 
@@ -8,21 +6,11 @@ const PER_PAGE = 12;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   const page = req.query.page || 1;
-  const KEY = `${keys.getMetaDeckList}?page=${page}`;
   try {
-    const exist = await redis.exists(KEY);
-    if (exist) {
-      const decks = await redis.get(KEY);
-      if (decks) {
-        res.setHeader('redis-cache', 'HIT');
-        return res.status(200).json(JSON.parse(decks));
-      }
-    }
     const temp = await pb.collection('meta_deck').getList(page, PER_PAGE, {
       sort: '-created',
       expand: 'items',
     });
-    await redis.set(KEY, JSON.stringify(temp));
     return res.status(200).json(temp);
   } catch (err) {
     console.error(err);
