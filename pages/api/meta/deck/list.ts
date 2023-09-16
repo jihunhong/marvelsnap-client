1a1f4007ab3f1ca8851939f2fc9359c295d8328b
jihunhong <redgee49@gmail.com>
2023-09-17 02:36:06 +0900

    fix: scroll-bar 수정

--- pages/api/meta/deck/list.ts
+++ pages/api/meta/deck/list.ts
@@ -1,3 +1,5 @@
+import redis from '@lib/redis';
+import keys from '@query/keys';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import PocketBase from 'pocketbase';
 
@@ -6,6 +8,15 @@ const PER_PAGE = 12;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   const page = req.query.page || 1;
+  const key = `${keys.getMetaDeckList}?page=${page}&perPage=${PER_PAGE}`;
+  const exist = await redis.exists(key);
+  if (exist) {
+    const decks = await redis.get(key);
+    if (decks) {
+      res.setHeader('redis-cache', 'HIT');
+      return res.status(200).json(JSON.parse(decks));
+    }
+  }
   try {
     const temp = await pb.collection('meta_deck').getList(page, PER_PAGE, {
       sort: '-created',
